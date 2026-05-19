25142eddfa87f2c474cff7ba009fc50555274731
baselsharaf <waelbk999@gmail>
2026-05-19 12:52:42 +0300

    create on generate route

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    // case CheckoutView.routeName:
+    //   return MaterialPageRoute(
+    //       builder: (context) => CheckoutView(
+    //             cartEntity: settings.arguments as CartEntity,
+    //           ));
+    // case BestSellingView.routeName:
+    //   return MaterialPageRoute(builder: (context) => const BestSellingView());
+    // case SigninView.routeName:
+    //   return MaterialPageRoute(builder: (context) => const SigninView());
+    // case SignupView.routeName:
+    //   return MaterialPageRoute(builder: (context) => const SignupView());
+    // case MainView.routeName:
+    //   return MaterialPageRoute(builder: (context) => const MainView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+  }
+}
---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:e_commerce/features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  static const routeName = 'splash';
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashViewBody();
+  }
+}
