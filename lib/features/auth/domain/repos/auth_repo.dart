61c3ca3742d80e4aaa3b5de2026a0a1aa156e4d7
baselsharaf <waelbk999@gmail>
2026-05-26 18:15:45 +0300

    add dart z package

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/errors/failures.dart';
+import 'package:e_commerce/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  );
+}
