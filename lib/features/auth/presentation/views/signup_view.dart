7f80ce07e0d989fb44d29eb404838fd9f64b52b7
baselsharaf <waelbk999@gmail>
2026-05-26 18:57:46 +0300

    add get it pcakge

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,6 +1,10 @@
+import 'package:e_commerce/core/services/get_it_service.dart';
 import 'package:e_commerce/core/widgets/custom_app_bar.dart';
+import 'package:e_commerce/features/auth/domain/repos/auth_repo.dart';
+import 'package:e_commerce/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import 'package:e_commerce/features/auth/presentation/views/widgets/signup_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignupView extends StatelessWidget {
   const SignupView({super.key});
@@ -8,9 +12,12 @@ class SignupView extends StatelessWidget {
   static const routeName = 'signup';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: const SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: const SignupViewBody(),
+      ),
     );
   }
 }
